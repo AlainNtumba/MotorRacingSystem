ee72e51796a43f16cd2e3b368a32e4531212e25d
AlainNtumba <50066005@mylife.unisa.ac.za>
2017-03-21 13:57:54 +0200

    Race Result Form

--- MotorRacingClubSystem/MotorRacingClubSystem/Form1.Designer.vb
+++ MotorRacingClubSystem/MotorRacingClubSystem/Form1.Designer.vb
@@ -41,17 +41,37 @@ Partial Class Form1
         Me.btnDeleteEvent = New System.Windows.Forms.Button()
         Me.btnAddEvent = New System.Windows.Forms.Button()
         Me.lstEvents = New System.Windows.Forms.ListBox()
+        Me.ResultsForm = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox7 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.cbRacerList = New System.Windows.Forms.ComboBox()
         Me.TabControl1.SuspendLayout()
         Me.DriverForm.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         Me.EventsForm.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
+        Me.ResultsForm.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
         Me.TabControl1.Controls.Add(Me.DriverForm)
         Me.TabControl1.Controls.Add(Me.EventsForm)
+        Me.TabControl1.Controls.Add(Me.ResultsForm)
         Me.TabControl1.Location = New System.Drawing.Point(4, 5)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
@@ -234,6 +254,175 @@ Partial Class Form1
         Me.lstEvents.Size = New System.Drawing.Size(487, 199)
         Me.lstEvents.TabIndex = 1
         '
+        'ResultsForm
+        '
+        Me.ResultsForm.Controls.Add(Me.TextBox2)
+        Me.ResultsForm.Controls.Add(Me.TextBox1)
+        Me.ResultsForm.Controls.Add(Me.Label7)
+        Me.ResultsForm.Controls.Add(Me.Label6)
+        Me.ResultsForm.Controls.Add(Me.Label5)
+        Me.ResultsForm.Controls.Add(Me.Label4)
+        Me.ResultsForm.Controls.Add(Me.Label3)
+        Me.ResultsForm.Controls.Add(Me.Label2)
+        Me.ResultsForm.Controls.Add(Me.Label1)
+        Me.ResultsForm.Controls.Add(Me.ComboBox7)
+        Me.ResultsForm.Controls.Add(Me.ComboBox6)
+        Me.ResultsForm.Controls.Add(Me.ComboBox5)
+        Me.ResultsForm.Controls.Add(Me.ComboBox4)
+        Me.ResultsForm.Controls.Add(Me.ComboBox3)
+        Me.ResultsForm.Controls.Add(Me.ComboBox2)
+        Me.ResultsForm.Controls.Add(Me.ComboBox1)
+        Me.ResultsForm.Controls.Add(Me.cbRacerList)
+        Me.ResultsForm.Location = New System.Drawing.Point(4, 22)
+        Me.ResultsForm.Name = "ResultsForm"
+        Me.ResultsForm.Size = New System.Drawing.Size(615, 215)
+        Me.ResultsForm.TabIndex = 2
+        Me.ResultsForm.Text = "Driver's Race Results "
+        Me.ResultsForm.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(346, 46)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(66, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(346, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(66, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(268, 100)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Enter Position"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(268, 181)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Enter Position"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(268, 127)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Enter Position"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(268, 154)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Enter Position"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(268, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Enter Position"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(268, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Enter Position"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(268, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Enter Position"
+        '
+        'ComboBox7
+        '
+        Me.ComboBox7.FormattingEnabled = True
+        Me.ComboBox7.Location = New System.Drawing.Point(141, 178)
+        Me.ComboBox7.Name = "ComboBox7"
+        Me.ComboBox7.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox7.TabIndex = 7
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Location = New System.Drawing.Point(141, 151)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox6.TabIndex = 6
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(141, 124)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox5.TabIndex = 5
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(141, 97)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox4.TabIndex = 4
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(141, 70)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox3.TabIndex = 3
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(141, 43)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(141, 16)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'cbRacerList
+        '
+        Me.cbRacerList.FormattingEnabled = True
+        Me.cbRacerList.Location = New System.Drawing.Point(13, 17)
+        Me.cbRacerList.Name = "cbRacerList"
+        Me.cbRacerList.Size = New System.Drawing.Size(121, 21)
+        Me.cbRacerList.TabIndex = 0
+        Me.cbRacerList.Tag = "Driver"
+        Me.cbRacerList.Text = "Select a Driver"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -249,6 +438,8 @@ Partial Class Form1
         Me.EventsForm.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
+        Me.ResultsForm.ResumeLayout(False)
+        Me.ResultsForm.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -272,4 +463,22 @@ Partial Class Form1
     Friend WithEvents btnDeleteEvent As Button
     Friend WithEvents btnAddEvent As Button
     Friend WithEvents lstEvents As ListBox
+    Friend WithEvents ResultsForm As TabPage
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox7 As ComboBox
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents cbRacerList As ComboBox
 End Class
